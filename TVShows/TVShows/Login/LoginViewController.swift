d1e5ec5041278eecf02cb270b9549dcca6b42053
CornyFlex <spyrofireice@gmail.com>
2019-07-08 22:20:07 +0200

    Added additional icon (button), start/stop button for Activity view. WORKING. Delay on entry, appear after 3 seconds.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -15,15 +15,49 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var button_icon: UIButton!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var button_outlet: UIButton!
+    @IBOutlet weak var welcome: UILabel!
+    @IBOutlet weak var button_start_outlet: UIButton!
     
     @IBAction func button(_ sender: UIButton) {
         count += 1
         label.text = "Number of taps: \(count)"
     }
+    //Game console - start animating
+    @IBAction func button_icon_action(_ sender: Any) {
+        Activity.stopAnimating()
+    }
+    //Moon - stop animating
+    @IBAction func button_start_action(_ sender: Any) {
+        Activity.startAnimating()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        button_outlet.layer.cornerRadius = 5
-        button_outlet.layer.borderWidth = 1
+        button_outlet.layer.cornerRadius = 25
+        button_outlet.layer.borderWidth = 5
+        button_start_outlet.layer.cornerRadius = 10
+        button_start_outlet.layer.borderWidth = 5
+        button_icon.layer.cornerRadius = 10
+        button_icon.layer.borderWidth = 5
+        
+        
+        label.isHidden = true
+        button_outlet.isHidden = true
+        button_icon.isHidden = true
+        button_start_outlet.isHidden = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3), execute: {
+        
+            self.label.isHidden = false
+            self.button_outlet.isHidden = false
+            self.button_icon.isHidden = false
+            self.button_start_outlet.isHidden = false
+        
+            self.Activity.stopAnimating()
+        })
     }
+    
+        
+        
 }
+
 
