909b4082e8b9e3be68ec3852c0bc3baa7746de31
CornyFlex <spyrofireice@gmail.com>
2019-07-08 17:07:53 +0200

    Corner radius, corner width

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -12,9 +12,15 @@ import UIKit
 class LoginViewController: UIViewController {
     var count = 0
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button_outlet: UIButton!
     @IBAction func button(_ sender: UIButton) {
         count += 1
         label.text = "Number of taps: \(count)"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        button_outlet.layer.cornerRadius = 5
+        button_outlet.layer.borderWidth = 1
     }
 }
 
