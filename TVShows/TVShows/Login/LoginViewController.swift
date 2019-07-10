49223c812e0401a5c2bbfb5ad685a88e425afbb0
CornyFlex <spyrofireice@gmail.com>
2019-07-10 22:00:53 +0200

    Layout working in portrait view, addded outlets/actions to code.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -11,6 +11,22 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var username_outlet: UITextField!
+    
+    @IBOutlet weak var password_outlet: UITextField!
+    
+    @IBOutlet weak var checkmark_outlet: UIButton!
+    
+    @IBOutlet weak var remember_outlet: UILabel!
+    
+    @IBOutlet weak var login_outlet: UIButton!
+    
+    @IBOutlet weak var create_outlet: UIButton!
+    
+    @IBOutlet weak var or_outlet: UILabel!
+    
+    @IBAction func checkmark_action(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         }
