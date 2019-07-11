e93a415619121b78a87737057f0c4a10ef143dc5
CornyFlex <spyrofireice@gmail.com>
2019-07-11 15:18:03 +0200

    Added checkbox function.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -14,29 +14,32 @@ class LoginViewController: UIViewController {
     // MARK - outlets:
     
     @IBOutlet weak var username_outlet: UITextField!
-    
     @IBOutlet weak var password_outlet: UITextField!
-    
     @IBOutlet weak var checkmark_outlet: UIButton!
-    
     @IBOutlet weak var remember_outlet: UILabel!
-    
     @IBOutlet weak var login_outlet: UIButton!
-    
     @IBOutlet weak var create_outlet: UIButton!
-    
     @IBOutlet weak var or_outlet: UILabel!
     
-    // MARK - acctions: 
+    // MARK: - life cycle functions
     
-    @IBAction func checkmark_action(_ sender: UIButton) {
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         }
+    
+    // MARK: - actions
+    
+    @IBAction func checkmark_action(_ sender: UIButton) {
+        if checkmark_outlet.currentImage == UIImage(named: "ic-checkbox-empty.png") {
+            checkmark_outlet.setImage(UIImage(named: "ic-checkbox-filled.png"), for: .normal)
+        }
+        else {
+            checkmark_outlet.setImage(UIImage(named: "ic-checkbox-empty.png"), for: .normal)
+        }
     }
+}
     
         
         
