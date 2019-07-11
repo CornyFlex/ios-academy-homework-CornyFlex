3c7bd062240ceb9744c0c77be87c47b78bc11e9b
CornyFlex <spyrofireice@gmail.com>
2019-07-11 08:22:58 +0200

    changes before switching

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -27,7 +27,7 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var or_outlet: UILabel!
     
-    // MARK - functions: 
+    // MARK - acctions: 
     
     @IBAction func checkmark_action(_ sender: UIButton) {
     }
