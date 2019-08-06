473093eff670b5bb1d4684f6ac370157cc7fb9e8
CornyFlex <spyrofireice@gmail.com>
2019-08-06 13:36:50 +0200

    Added shadows for thumbnail pictures :)

--- TVShows/TVShows/Home/Cells/ShowsCollectionViewCell.swift
+++ TVShows/TVShows/Home/Cells/ShowsCollectionViewCell.swift
@@ -29,6 +29,17 @@ extension ShowsCollectionViewCell {
         let url = URL(string: "https://api.infinum.academy" + item.imageUrl)
         let placeholderImage = UIImage(named: "Image-5")
         thumbnailTVShow.kf.setImage(with: url, placeholder: placeholderImage)
+        addShadowToImage()
+    }
+}
+
+extension ShowsCollectionViewCell {
+    
+    func addShadowToImage() {
+        thumbnailTVShow.layer.shadowColor = UIColor.black.cgColor
+        thumbnailTVShow.layer.shadowOffset = CGSize(width: 5, height: 5)
+        thumbnailTVShow.layer.shadowRadius = 5
+        thumbnailTVShow.layer.shadowOpacity = 1.0
     }
 }
 
